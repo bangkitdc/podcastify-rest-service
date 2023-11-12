f2617ba16c09c34cf00e9d5f583427566d644ca4
Jimly-Firdaus <13521102@std.stei.itb.ac.id>
2023-11-12 16:14:36 +0700

    chore: add middleware checker

--- src/routes/subscription.route.ts
+++ src/routes/subscription.route.ts
@@ -7,6 +7,7 @@ import {
 } from '../dto';
 import { SubscriptionService } from '../services';
 import { SubscriptionController } from '../controllers';
+import { AuthMiddleware } from '../middlewares';
 
 const subscriptionService = new SubscriptionService();
 const subscriptionController = new SubscriptionController(subscriptionService);
@@ -16,17 +17,18 @@ const subscriptionRoute = Router();
 subscriptionRoute
   .patch(
     '/subscription',
+    AuthMiddleware.authenticateToken,
     RequestHelper.validate(approveSubscriptionSchema),
     RequestHelper.exceptionGuard(subscriptionController.approveSubscription),
   )
   .get(
     '/subscription',
-    RequestHelper.exceptionGuard(
-      subscriptionController.getAllSubscriptions,
-    ),
+    AuthMiddleware.authenticateToken,
+    RequestHelper.exceptionGuard(subscriptionController.getAllSubscriptions),
   )
   .get(
     '/subscription/:subscriber_id',
+    AuthMiddleware.authenticateToken,
     RequestHelper.validate(getAllSubscriptionBySubscriberIdSchema),
     RequestHelper.exceptionGuard(
       subscriptionController.getAllSubscriptionBySubscriberID,
