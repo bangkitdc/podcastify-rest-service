334816c85138fd79da57a75c1fc7857ff175fab3
Muhammad Bangkit Dwi Cahyono <bangkitdc@gmail.com>
2023-11-15 18:18:47 +0700

    feat(env): add env

--- src/configs/jwt.ts
+++ src/configs/jwt.ts
@@ -1,5 +1,5 @@
 const DEFAULT_ACCESS_TOKEN = 'podcastify_default_access_secret_key';
-const DEFAULT_ACCESS_TOKEN_EXPIRATION = 3600; // 1 hour
+const DEFAULT_ACCESS_TOKEN_EXPIRATION = 60 * 15; // 15 minutes
 
 const DEFAULT_REFRESH_TOKEN = 'podcastify_default_access_refresh_key';
 const DEFAULT_REFRESH_TOKEN_EXPIRATION = 3600 * 24 * 7; // 7 days
@@ -13,7 +13,7 @@ const jwtAccessTokenExpiration = jwtAccessTokenExpirationString
 
 const jwtRefreshToken = (process.env.REFRESH_TOKEN_SCRET as string) || DEFAULT_REFRESH_TOKEN;
 
-const jwtRefreshTokenExpirationString = process.env.JWT_TIME_REFRESH_EXPIRATION;
+const jwtRefreshTokenExpirationString = process.env.REFRESH_TOKEN_EXPIRATION;
 const jwtRefreshTokenExpiration = jwtRefreshTokenExpirationString
   ? parseInt(jwtRefreshTokenExpirationString, 10)
   : DEFAULT_REFRESH_TOKEN_EXPIRATION;
